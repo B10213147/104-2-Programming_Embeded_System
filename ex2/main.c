9ab28f3c043afb279e02ccbe8596c349e5640c3f
B10213147 <B10213147@yuntech.org.tw>
2016-03-23 23:57:27 +0100

    little change

--- ex2/main.c
+++ ex2/main.c
@@ -73,8 +73,10 @@ int main()
 }
 
 void lcd_driver(){
-	int i, done=0;
-	lcd_write_comm(DDRAM | 0x00);
+  int i, done=0;
+
+  lcd_write_comm(DDRAM | 0x00);
+
 	for(i = 0; i < 16; i++){
     if(lcd_string[i]=='\0')
       done=1;
@@ -83,7 +85,9 @@ void lcd_driver(){
     else
       lcd_write_data(lcd_string[i]);
   }
+
 	lcd_write_comm(DDRAM | 0x40);
+
 	for(; i < 32; i++){
     if(lcd_string[i]=='\0')
       done=1;
