acbec5da6c70b222d093edfbd599a5c79ddf34ef
B10213147 <B10213147@yuntech.org.tw>
2016-05-05 11:38:54 +0200

    test uart_driver

--- ex7/main.c
+++ ex7/main.c
@@ -3,16 +3,51 @@
 #include "lcd_driver.h"
 #include "rtos2.h"
 
+void str2lcd(int position, char *str);
+
 extern char *lcd_string;
 
 int main()
 {
-    char c;
+    char str[10];
+    int n, num=0;
+/*
+    rtos2task_create(keys_driver, NULL,	1, 10);
+    rtos2task_create(lcd_driver, NULL,	2, 2);
+    rtos2task_create(uart_driver, NULL,	3, 1);
+*/
     while(1){
-        keys_driver();
-        lcd_string = keys_pipe->buffer;
+        //test uart_driver
+        uart_driver();
+
+        n = rtos2pipe_read(uart_rx_pipe, str, 1);
+        if(n == 1){
+            lcd_string[num] = str[0];
+            lcd_string[num+1] = '\0';
+            num++;
+
+            str[n] = '\n';
+            n = rtos2pipe_write(uart_tx_pipe, str, 2);
+        }
+
         lcd_driver();
-        rtos2pipe_read(keys_pipe, &c, 1);
+
+        if(num == 32){
+            str2lcd(0, lcd_string+16);
+            str2lcd(16, "                ");
+            //str2lcd(16, "");
+            num = 16;
+        }
     }
+
     return 0;
+}
+
+void str2lcd(int position, char *str){
+    int i;
+
+    for(i=0; position+i >= 0 && position+i < 32; i++){
+        if(str[i] == '\0') break;
+        lcd_string[position+i] = str[i];
+    }
 }
--- ex7/uart_driver.c
+++ ex7/uart_driver.c
@@ -27,3 +27,4 @@ void uart_driver(){
         }
 	}
 }
+
