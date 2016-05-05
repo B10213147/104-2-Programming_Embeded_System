e274eef2dbc9fc8937cda19f22ba7e34f6869af2
B10213147 <B10213147@yuntech.org.tw>
2016-05-05 19:30:57 +0200

    ex2

--- ex2/startup.c
+++ ex2/startup.c
@@ -5,23 +5,22 @@ extern int main();
 
 void start_up()
 {
-  
-  int input = P0_12 | P0_13 | P0_14 | P0_15;          // glej gpio.h
-  int output = P0_4; // glej gpio.h
-  int clock_mhz = 12;                     // glej definicijo funk. init v init.c
-  int vpbdiv = cclk_4;                    // glej vpb.h  
-  int value = 0;                          // glej definicijo funk. init v init.c
-  
-  init(clock_mhz, vpbdiv, input, output, value);
-  
-  int prescale = 0;
-  int match[4] = {0,0,0,0};
-  int control = 0;
-  int count = timer;
-  
-  timer1_init(prescale, match, control, count);
-  T1TCR = counter_enable;
-  
-	main();	// Start main program
-	while(1);
+    //cpu initialization
+    int input = P0_12 | P0_13 | P0_14 | P0_15;
+    int output = P0_4;
+    int clock_mhz = 12;
+    int vpbdiv = cclk_4;
+    int value = 0;
+    init(clock_mhz, vpbdiv, input, output, value);
+
+    //timer initialization
+    int prescale = 0;
+    int match[4] = {0,0,0,0};
+    int control = 0;
+    int count = timer;
+    timer1_init(prescale, match, control, count);
+    T1TCR = counter_enable;
+
+    main();	// Start main program
+    while(1);
 }
